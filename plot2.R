fcc6ce755b246c168e57ccb5d062f29f35787729
sagc13 <silviagarciacabrera@hotmail.com>
2020-08-08 20:52:43 -0500

    Updates

--- plot2.R
+++ plot2.R
@@ -1,15 +1,18 @@
-#Plot 1
+#Plot 2
 library(dplyr)
 library(lubridate)
 
+#Download File
 fileZIP <- "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(fileZIP, destfile = "Dataset.zip")
 unzip("Dataset.zip")
 
+#Filtering data based on dates provided
 Dataset <- read.table("household_power_consumption.txt",header= TRUE,sep=";",na.strings="?")
 Dataset$Date <- as.Date(Dataset$Date,format="%d/%m/%Y")
 Data <- filter(Dataset, Date >= "2007-02-01" & Date <= "2007-02-02")
 
+#Generating png and plot
 png("plot2.png", width = 480, height = 480)
 datetime <- paste(as.Date(Data$Date), Data$Time)
 Data$Datetime <- as.POSIXct(datetime)
